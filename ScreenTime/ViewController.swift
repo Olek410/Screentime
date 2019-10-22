ef92eb1f80a143fa706c3133043b22402a9edd1f
Olivier Balazy <olek5622@gmail.com>
2019-10-22 14:54:50 -0500

    Initial Commit

---
+++ ScreenTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenTime
+//
+//  Created by Olivier Balazy on 10/22/19.
+//  Copyright © 2019 Olivier Balazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
