62434a0ab63ffb14f51e6bec453faa2f0d21976b
Olek410 <olek5622@gmail.com>
2020-02-12 02:57:07 -0600

    update
    Added shake feature(still to find a way for it to randomize and choose a new game on the detail screen) and counters for players is almost complete

--- ScreenTime/GameDetalViewController.swift
+++ ScreenTime/GameDetalViewController.swift
@@ -9,13 +9,39 @@
 import UIKit
 import WebKit
 
-class GameDetalViewController: UIViewController {
+class GameDetalViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var gameTitle: UILabel!
     @IBOutlet weak var materialsInfo: UILabel!
     @IBOutlet weak var numberOfPlayers: UILabel!
     @IBOutlet weak var rulesInfo: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addPlayer: UIButton!
+    
+    @IBOutlet var scoreCellsUI: [UIView]!
+    
+    @IBOutlet var scoreCellName: [UITextField]!
+    @IBOutlet var scoreCellScore: [UILabel]!
+    
+    @IBOutlet weak var TotalView: UIView!
+    
+    var currentPlayerAmount = 0
+    
+    struct playerScoreCell {
+        var name: String
+        var score: Int
+        var hidden: Bool
+        var index: Int
+    }
+    
+    var playerScores: [playerScoreCell] = []
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        pushScoreName(index: textField.tag,text: textField.text ?? "")
+        return true
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +57,91 @@ class GameDetalViewController: UIViewController {
         numberOfPlayers.text = "Number of Players: \(overallCurrentGame.playerAmountString)"
         
         rulesInfo.text = overallCurrentGame.rules
+        
+        for i in 0...4 {
+            scoreCellName[i].delegate = self
+            playerScores.append(playerScoreCell(name: "", score: 0, hidden: true, index: i))
+        }
+        hideScoreCells()
+    }
+    
+    func hideScoreCells(){
+        var i = 0
+        for _ in playerScores{
+                if(i<currentPlayerAmount){
+                    scoreCellsUI[i].isHidden = false
+                }
+                else{
+                    scoreCellsUI[i].isHidden = true
+                }
+                i+=1
+            }
+    }
+    
+    func pushScoreName(index: Int, text: String){
+        playerScores[index].name = text
+        print(text)
+    }
+    
+    func updateScoreName(){
+        var i = 0
+        for textField in scoreCellName{
+            textField.text = playerScores[i].name
+            i+=1
+        }
+    }
+    
+    func updateScoreCellScore(){
+        var i = 0
+        for cellScore in scoreCellScore{
+            cellScore.text = String(playerScores[i].score)
+            i+=1
+        }
+    }
+    
+    @IBAction func addPlayerPress(_ sender: Any) {
+        if currentPlayerAmount<4 {
+            currentPlayerAmount+=1
+            addPlayer.isEnabled=true
+            hideScoreCells()
+        }
+        else if currentPlayerAmount==4{
+            currentPlayerAmount+=1
+            addPlayer.isEnabled=false
+            hideScoreCells()
+        }
+        else{
+            addPlayer.isEnabled=false
+        }
+        
     }
 
+    @IBAction func subtract(_ sender: UIButton) {
         
+        (playerScores[(sender.tag)/2].score) -= 1
+        updateScoreCellScore()
+        
+    }
     
+    @IBAction func add(_ sender: UIButton) {
+        
+        playerScores[((sender.tag)-1)/2].score += 1
+        updateScoreCellScore()
+        
+    }
+    
+    @IBAction func removePlayerAction(_ sender: UIButton) {
+        
+        playerScores.remove(at: sender.tag)
+        
+        playerScores.append(playerScoreCell(name: "", score: 0, hidden: true, index: sender.tag))
+        scoreCellName[playerScores.count-1].delegate = self
+        currentPlayerAmount-=1
+        hideScoreCells()
+        updateScoreCellScore()
+        updateScoreName()
+        
+    }
     
 }
     
